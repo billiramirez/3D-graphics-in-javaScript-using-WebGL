040de1aa4a395a5ff0c5c02d3e5d44662f97725d
Billi Ramirez <ramirezbilly42@outlook.com>
2021-11-04 12:23:02 -0600

    loading in webgl shaders

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-var gl;
-var shaderProgram;
-var vertices;
+var gl, shaderProgram, vertices;
 
 initGL();
 createShaders();
@@ -8,38 +6,16 @@ createVertices();
 draw();
 
 function initGL() {
-  const canvas = document.getElementById("canvas");
+  var canvas = document.getElementById("canvas");
+  console.log(canvas);
   gl = canvas.getContext("webgl");
   gl.viewport(0, 0, canvas.width, canvas.height);
   gl.clearColor(1, 1, 1, 1);
 }
+
 function createShaders() {
-  const vs = `
-  attribute vec4 coords;
-  attribute float pointSize;
-
-    void main (void) {
-      gl_Position = coords;
-      gl_PointSize = pointSize;
-    }
-  `;
-
-  const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-  gl.shaderSource(vertexShader, vs);
-  gl.compileShader(vertexShader);
-
-  const fs = `
-    precision mediump float;
-    uniform vec4 color;
-
-    void main(void) {
-      gl_FragColor = color;
-    }
-  `;
-
-  const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-  gl.shaderSource(fragmentShader, fs);
-  gl.compileShader(fragmentShader);
+  var vertexShader = getShader(gl, "shader-vs");
+  var fragmentShader = getShader(gl, "shader-fs");
 
   shaderProgram = gl.createProgram();
   gl.attachShader(shaderProgram, vertexShader);
@@ -49,29 +25,72 @@ function createShaders() {
 }
 
 function createVertices() {
-  vertices = [
-    -0.9, 0.5, 0, -0.7, -0.5, 0, -0.5, 0.5, 0, -0.3, -0.5, 0, -0.1, 0.5, 0, 0.1,
-    -0.5, 0, 0.3, 0.5, 0, 0.5, -0.5, 0, 0.7, 0.5, 0, 0.9, -0.5, 0,
-  ];
+  vertices = [-0.9, -0.9, 0.0, 0.9, -0.9, 0.0, 0.0, 0.9, 0.0];
 
   var buffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
-  const coords = gl.getAttribLocation(shaderProgram, "coords");
-  // gl.vertexAttrib3f(coords, 0.5, 0.5, 0);
+  var coords = gl.getAttribLocation(shaderProgram, "coords");
+  //   gl.vertexAttrib3f(coords, 0.5, 0.5, 0);
   gl.vertexAttribPointer(coords, 3, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(coords);
   gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
-  const pointSize = gl.getAttribLocation(shaderProgram, "pointSize");
+  var pointSize = gl.getAttribLocation(shaderProgram, "pointSize");
   gl.vertexAttrib1f(pointSize, 20);
 
-  const color = gl.getUniformLocation(shaderProgram, "color");
+  var color = gl.getUniformLocation(shaderProgram, "color");
   gl.uniform4f(color, 0, 0, 0, 1);
 }
 
 function draw() {
   gl.clear(gl.COLOR_BUFFER_BIT);
-  gl.drawArrays(gl.LINE_STRIP, 0, 10);
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
+}
+
+/*
+ * https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Tutorial/Adding_2D_content_to_a_WebGL_context
+ */
+function getShader(gl, id) {
+  var shaderScript, theSource, currentChild, shader;
+
+  shaderScript = document.getElementById(id);
+
+  if (!shaderScript) {
+    return null;
+  }
+
+  theSource = "";
+  currentChild = shaderScript.firstChild;
+
+  while (currentChild) {
+    if (currentChild.nodeType == currentChild.TEXT_NODE) {
+      theSource += currentChild.textContent;
+    }
+
+    currentChild = currentChild.nextSibling;
+  }
+  if (shaderScript.type == "x-shader/x-fragment") {
+    shader = gl.createShader(gl.FRAGMENT_SHADER);
+  } else if (shaderScript.type == "x-shader/x-vertex") {
+    shader = gl.createShader(gl.VERTEX_SHADER);
+  } else {
+    // Unknown shader type
+    return null;
+  }
+  gl.shaderSource(shader, theSource);
+
+  // Compile the shader program
+  gl.compileShader(shader);
+
+  // See if it compiled successfully
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    alert(
+      "An error occurred compiling the shaders: " + gl.getShaderInfoLog(shader)
+    );
+    return null;
+  }
+
+  return shader;
 }
